74543e46c139fa0d4f2e51c06614f555c0c756f7
rakibahmad007 <118014776+rakibahmad007@users.noreply.github.com>
2025-02-11 17:08:06 +0600

    final commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import *  as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 const renderer = new THREE.WebGLRenderer({antialias: true})
 // to give the edges a bit of smooth effect we are using antialias
@@ -7,6 +9,9 @@ renderer.outputColorSpace = THREE.SRGBColorSpace;
 renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.setClearColor(0x000000)
 renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
 document.body.appendChild(renderer.domElement)
 
@@ -18,6 +23,19 @@ const camera = new THREE.PerspectiveCamera(
     1,
     1000
 )
+camera.position.set(4,5,11);
+
+const controls = new OrbitControls(camera,renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 5;
+controls.maxDistance = 20;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 1, 0);
+controls.update();
+
 
 const groundGeometry = new THREE.PlaneGeometry(20,20,32,32)
 groundGeometry.rotateX(-Math.PI/2)
@@ -29,15 +47,45 @@ const groundMaterial = new THREE.MeshStandardMaterial({
 })
 
 const groundMesh =  new THREE.Mesh(groundGeometry, groundMaterial)
+groundMesh.castShadow = false;
+groundMesh.receiveShadow = true;
 scene.add(groundMesh)
 
-const spotLight = new THREE.SpotLight(0xffffff,3,100,0.2,0.5);
-spotLight.position.set(0,25,0)
-scene.add(spotLight)
+const spotLight = new THREE.SpotLight(0xffffff, 3000, 1000, 0.22, 1);
+spotLight.position.set(0, 25, 0);
+spotLight.castShadow = true;
+spotLight.shadow.bias = -0.0001;
+scene.add(spotLight);
+
+const loader = new GLTFLoader().setPath('public/porsche_911_930_turbo_1975/')
+loader.load('scene.gltf', (gltf) => {
+    const mesh = gltf.scene;
+    
+    mesh.traverse((child) => {
+        if (child.isMesh) {
+            child.castShadow = true
+            child.receiveShadow = true
+        }
+    });
+
+    mesh.position.set(-0.5,1.6,-1)
+    scene.add(mesh);
+
+    // Ensure the spotlight is positioned above the car
+    const carPosition = mesh.position; // Get car's position
+
+    // Position spotlight directly above the car
+    spotLight.position.set(carPosition.x, 25, carPosition.z); // Adjust the height (25) to place above the car
+    
+    // Set spotlight's target to the car's position
+    spotLight.target.position.set(carPosition.x, carPosition.y, carPosition.z);
+    scene.add(spotLight.target); // Ensure the target is added to the scene
+});
 
 function animate(){
 
     requestAnimationFrame(animate)
+    controls.update();
     renderer.render(scene,camera);
 }
 
